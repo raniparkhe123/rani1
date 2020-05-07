f6beff7f9a62603e5afaa60096bf8c991904318c
raniparkhe123 <46881854+raniparkhe123@users.noreply.github.com>
2020-05-07 13:40:06 +0530

    Add files via upload

---
+++ DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ViewBag["Countries"] = new List<string>
+            {
+                "India",
+                "US",
+                "UK",
+                "Canada"
+            };
+            return View();
+        }
+    }
+}
