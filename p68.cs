60842a250170aebc9f722a3872225ddefc9ec604
raniparkhe123 <46881854+raniparkhe123@users.noreply.github.com>
2020-05-18 14:22:41 +0530

    Add files via upload

---
+++ p68.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Method1()
+        {
+            return "<h1>Method1 Invoked</h1>";
+        }
+
+        [NonAction] //1st way 
+
+        /*private string Method2()//2nd way*/
+        public string Method2()
+        {
+            return "<h1>Method2 Invoked</h1>";
+        }
+
+    }
+}
