c4809bfabe0ebc332250859f9e0b8cc880c2fe49
raniparkhe123 <46881854+raniparkhe123@users.noreply.github.com>
2020-05-18 19:25:28 +0530

    Add files via upload

---
+++ p71.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class ChildController : Controller
+    {
+        // GET: Child
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Countries(List<string> countryNames)
+        {
+            return View(countryNames);
+        }
+
+    }
+}
