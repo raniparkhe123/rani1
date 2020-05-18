18c81b782ca708dca4f36dd61168769dccb8a036
raniparkhe123 <46881854+raniparkhe123@users.noreply.github.com>
2020-05-18 14:58:50 +0530

    Add files via upload

---
+++ p69.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    [Authorize]
+    public class Home1Controller : Controller
+    {
+        // GET: Home1
+
+        
+        public ActionResult SecureMethod()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult NonSecureMethod()
+        {
+            return View();
+        }
+    }
+}
